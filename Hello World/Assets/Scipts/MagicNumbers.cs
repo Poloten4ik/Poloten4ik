dffb5f10ee9dbb2287330afd60caa08826d4c726
Artem <krekotnev@gmail.com>
2020-09-22 21:07:16 +0300

    Initial commit

---
+++ Hello World/Assets/Scipts/MagicNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+{
+    public int min ;
+    public int max ;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //min = 1;
+        //max = 1000;
+
+        print("Загадайте число от " + min + " до " + max);
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return ) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            print("Игра закончена");
+            Start();
+
+        }
+    }
+    void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print("Ваше число: " + guess + " ?");
+    }
+}
---
+++ Hello World/Assets/Scipts/NumbersTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbersTest : MonoBehaviour
+{
+    int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+    }
+
+    // update is called once per frame
+    void update()
+    {
+        number += 1;
+        print(number);
+
+    }
+}
